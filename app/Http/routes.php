f1596d717705ced3b8bf600242bbdf67f47cae08
kmizzi <kalvin@mizzi.com>
2016-01-22 15:08:17 -0800

    test again

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,18 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 use App\Task;
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
