b9b5c04662c32411bd28b971e121afb6f48892f3
kmizzi <kalvin@mizzi.com>
2016-01-22 15:05:10 -0800

    fix routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,37 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    $tasks => Task::orderBy('created_at', 'asc')->get();
-
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
-
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
-Route::delete('/task/{task}', function (Task $task) {
-    $task->delete();
-
-    return redirect('/');
-});
+use App\Task;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -55,5 +26,37 @@ Route::delete('/task/{task}', function (Task $task) {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+    /**
+     * Show Task Dashboard
+     */
+
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
+    });
+
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    Route::delete('/task/{task}', function (Task $task) {
+        $task->delete();
+
+        return redirect('/');
+    });
 });
