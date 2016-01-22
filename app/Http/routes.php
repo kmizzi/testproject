86fb32e92b2f275dc022f76c5419cf424b118f05
kmizzi <kalvin@mizzi.com>
2016-01-22 14:48:58 -0800

    Added task route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,10 +12,21 @@
 */
 
 Route::get('/', function () {
-//    return view('welcome');
+    return view('tasks');
+});
 
-//return 1+1;
-return view('tasks');
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Create The Task...
 });
 
 /*
