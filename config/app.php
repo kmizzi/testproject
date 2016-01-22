e33497343a38073bd77d63041557a11cd946192d
kmizzi <kalvin@mizzi.com>
2016-01-22 15:21:39 -0800

    set database info

--- config/app.php
+++ config/app.php
@@ -18,11 +18,11 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
-    |--------------------------------------------------------------------------
+|--------------------------------------------------------------------------
     |
     | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
+| stack traces will be shown on every error that occurs within your
+| application. If disabled, a simple generic error page is shown.
     |
     */
 
